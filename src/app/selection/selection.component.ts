ac0603c3c5ef3222f8f32f570bd6bf7e9d345eb0
Trapflex <Marvinurbanik@gmail.com>
2025-03-20 00:37:22 +0100

    Initial commit

---
+++ src/app/selection/selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'td-selection',
+  imports: [],
+  templateUrl: './selection.component.html',
+  styleUrl: './selection.component.css'
+})
+export class SelectionComponent {
+
+}
