8114ff93946ab0f60029d8545eb35b3bb6b749c1
Trapflex <Marvinurbanik@gmail.com>
2025-03-20 01:55:32 +0100

    Header erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FilterComponent } from "./filter/filter.component";
+import { SelectionComponent } from "./selection/selection.component";
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeaderComponent, FilterComponent, SelectionComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
