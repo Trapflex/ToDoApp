406c4bcb7032f8155319190bf13ae20255c51f79
Trapflex <Marvinurbanik@gmail.com>
2025-03-20 09:50:46 +0100

    Projekt bis auf Speichern vollständig

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'td-filter',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  @Output() filterChanged = new EventEmitter<string>();
 
+  setFilter(filter: string) {
+    this.filterChanged.emit(filter);
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'td-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  todos = [
-    { text: 'Learn Angular' },
-    { text: 'Build a ToDo App' },
-    { text: 'Deploy the App' }
+  todos: { text: string, completed: boolean }[] = [
+    
   ];
+
+  filteredTodos: { text: string, completed: boolean }[] = [...this.todos];
+
   addTodo(newTodoText: string) {
-    this.todos.push({ text: newTodoText });
+    this.todos.push({ text: newTodoText, completed: false });
+    this.applyFilter('all');
   }
-  deleteTodo(todo: { text: string }) {
+
+  deleteTodo(todo: { text: string, completed: boolean }) {
     this.todos = this.todos.filter(t => t !== todo);
+    this.applyFilter('all');
+  }
+
+  applyFilter(filter: string) {
+    if (filter === 'completed') {
+      this.filteredTodos = this.todos.filter(todo => todo.completed);
+    } else if (filter === 'incompleted') {
+      this.filteredTodos = this.todos.filter(todo => !todo.completed);
+    } else {
+      this.filteredTodos = [...this.todos];
+    }
   }
 }
